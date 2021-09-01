835b4c8dadf7056f6ef87338847393ecca1d89b9
Yusuf Erkan DARIYEMEZ <yusuf_erkamm@hotmail.com>
2021-09-01 22:53:49 +0300

    Inhterface

--- Interface/Program.cs
+++ Interface/Program.cs
@@ -10,6 +10,56 @@ namespace Interface
     {
         static void Main(string[] args)
         {
+            PersonManager manager = new PersonManager();
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Yusuf",
+                LastName = "Dariyemez",
+                Adress = "Istanbul"
+            };
+            Student student = new Student
+            {
+
+                Id = 1,
+                FirstName = "Erkan",
+                LastName = "Dariyemez",
+                Departman = "Computer Science"
+            };
+            manager.Add(customer);
+            manager.Add(student);
+            Console.ReadLine();
+        }
+
+        interface IPerson
+        {
+            int Id { get; set; }
+            string FirstName { get; set; }
+            string LastName { get; set; }
+        }
+
+        class Customer : IPerson
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Adress { get; set; }
+        }
+
+        class Student:IPerson
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Departman { get; set; }
+        }
+
+        class PersonManager
+        {
+            public void Add(IPerson person)
+            {
+                Console.WriteLine(person.FirstName);
+            }
         }
     }
 }
