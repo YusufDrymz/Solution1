d976a1ef4a1f1c074ee4465a6d255453cf1bc02e
YusufDrymz <77965724+YusufDrymz@users.noreply.github.com>
2021-09-08 20:21:00 +0300

    pluggin

---
+++ PluginInterface/IPluggin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginInterface
+{
+    public interface IPlugin
+    {
+        string Guid { get; }
+        string Name { get; }
+        void Start();
+    }
+}
