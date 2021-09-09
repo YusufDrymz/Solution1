05ee061ba82fdc8d9bfaefd66f8f355e1fcd7e7a
YusufDrymz <77965724+YusufDrymz@users.noreply.github.com>
2021-09-09 18:56:04 +0300

    Add&Remove

--- DenemeII/Program.cs
+++ DenemeII/Program.cs
@@ -4,17 +4,99 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-namespace DenemeII
+namespace AddveRemoveErisim
 {
+    delegate void OlayYoneticisi();
+    class Buton
+    {
+        OlayYoneticisi[] olay = new OlayYoneticisi[2];
+        public event OlayYoneticisi ButonKlik
+        {
+            add
+                
+            {
+                int i;
+                for (i = 0; i < 2; ++i)
+                    if (olay[i] == null)
+                    {
+                        olay[i] = value;
+                        break;
+                    }
+                if (i== 2)
+                        Console.WriteLine("Olay Yoneticisine En Fazla 2 Metot Eklenebilr.");
+            }
+            remove
+            {
+                int i;
+                for (i = 0; i < 2; ++i)
+                    if (olay[i] == null)
+                    {
+                        olay[i] = value;
+                        break;
+                    }
+                if (i== 2)
+                        Console.WriteLine("Metot Bulunumadı");
+            }
+        }
+        public void Kliklendi()
+        {
+            for (int i = 0; i < 2; ++i)
+                if (olay[i] != null)
+                    olay[i]();           
+        }
+    }
+    class Pencere
+    {
+        int PencereNo;
+        public Pencere(int no)
+        {
+            PencereNo = no;
+        }
+        public void ButonKlik()
+        {
+            Console.WriteLine("{0} nolu pencere olayı algıladı.", PencereNo);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            int a = 50;
-            string str = String.Format("{0:C}", a);
-            Console.WriteLine(str);
+            Buton buton = new Buton();
+            Pencere p1 = new Pencere(1);
+            Pencere p2 = new Pencere(2);
+
+            //Geçerli Ekleme
+            buton.ButonKlik += new OlayYoneticisi(ButonKlik);
+            buton.Kliklendi();
+            Console.WriteLine();
+
+            //Geçerli Ekleme
+            buton.ButonKlik += new OlayYoneticisi(p1.ButonKlik);
+            buton.Kliklendi();
+            Console.WriteLine();
+
+            //Geçerli Ekleme
+            buton.ButonKlik += new OlayYoneticisi(p2.ButonKlik);
+            buton.Kliklendi();
+            Console.WriteLine();
+
+            buton.ButonKlik -= new OlayYoneticisi(p1.ButonKlik);
+            buton.Kliklendi();
+            Console.WriteLine();
+
+            buton.ButonKlik -= new OlayYoneticisi(ButonKlik);
+            buton.Kliklendi();
+            Console.WriteLine();
+
+            buton.ButonKlik -= new OlayYoneticisi(ButonKlik);
+            buton.Kliklendi();
+            Console.WriteLine();
 
             Console.ReadLine();
+        }
+        public static void ButonKlik()
+        {
+            Console.WriteLine("Statik Metot Olayı Algılandı..");
         }
     }
 }
