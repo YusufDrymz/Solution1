18e7e0a1896b5eb175de48fa3ceeef382a1bab74
YusufDrymz <77965724+YusufDrymz@users.noreply.github.com>
2021-09-10 14:31:53 +0300

    contunie

---
+++ Carpma/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Islem;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpma
+{
+    public class Carpma : IHesap
+    {
+        public int Hesapla(int a, int b)
+        {
+            return a * b;
+        }
+        public string Isim
+        {
+            get
+            {
+                return "Carpma";
+            }
+        }
+    }
+}
---
+++ Islem/Islem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Islem
+{
+    public interface IHesap
+    {
+        int Hesapla(int a, int b);
+        string Isim { get; }
+    }
+}
